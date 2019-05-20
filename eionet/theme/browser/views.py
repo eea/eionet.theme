9786261709142fba671d56bbfddf9eca4f64691d
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-05-20 17:29:58 +0300

    Added frontpage helper views.

--- eionet/theme/browser/views.py
+++ eionet/theme/browser/views.py
@@ -124,6 +124,127 @@ class CollectionHelperView(BrowserView):
         types_used = registry.get('plone.types_use_view_action_in_listings', [])
         return types_used
 
+    @property
+    def plone_view(self):
+        if not self._plone_view:
+            self._plone_view = getMultiAdapter(
+                (self.context, self.request),
+                name=u'plone'
+            )
+        return self._plone_view
+
+    def normalizeString(self, text):
+        return self.plone_view.normalizeString(text)
+
+
+class FrontpageNewsHelperView(BrowserView):
+    """ Custom class for collection helper view
+    """
+    text_class = None
+    _plone_view = None
+    _portal_state = None
+    _pas_member = None
+
+    def tabular_fields(self):
+        """ Returns a list of all metadata fields from the catalog that were
+            selected.
+        """
+        context = aq_inner(self.context)
+        wrapped = ICollection(context)
+        fields = wrapped.selectedViewFields()
+        fields = [field[0] for field in fields]
+
+        return fields
+
+    @property
+    def plone_view(self):
+        if not self._plone_view:
+            self._plone_view = getMultiAdapter(
+                (self.context, self.request),
+                name=u'plone'
+            )
+        return self._plone_view
+
+    @property
+    def collection_behavior(self):
+        return ICollection(aq_inner(self.context))
+
+    @property
+    def b_size(self):
+        return getattr(self, '_b_size', self.collection_behavior.item_count)
+
+    @property
+    def b_start(self):
+        b_start = getattr(self.request, 'b_start', None) or 0
+        return int(b_start)
+
+    def results(self, **kwargs):
+        """Return a content listing based result set with results from the
+        collection query.
+        :param **kwargs: Any keyword argument, which can be used for catalog
+                         queries.
+        :type  **kwargs: keyword argument
+        :returns: plone.app.contentlisting based result set.
+        :rtype: ``plone.app.contentlisting.interfaces.IContentListing`` based
+                sequence.
+        """
+        # Extra filter
+        contentFilter = dict(self.request.get('contentFilter', {}))
+        contentFilter.update(kwargs.get('contentFilter', {}))
+        kwargs.setdefault('custom_query', contentFilter)
+        kwargs.setdefault('batch', True)
+        kwargs.setdefault('b_size', self.b_size)
+        kwargs.setdefault('b_start', self.b_start)
+
+        results = self.collection_behavior.results(**kwargs)
+        return results
+
+    def batch(self):
+        # collection is already batched.
+        return self.results()
+
+    def tabular_fielddata(self, item, fieldname):
+        value = getattr(item, fieldname, '')
+        if safe_callable(value):
+            value = value()
+        if fieldname in [
+                'CreationDate',
+                'ModificationDate',
+                'Date',
+                'EffectiveDate',
+                'ExpirationDate',
+                'effective',
+                'expires',
+                'start',
+                'end',
+                'created',
+                'modified',
+                'last_comment_date']:
+            value = self.context.toLocalizedTime(value, long_format=1)
+
+        return {
+            # 'title': _(fieldname, default=fieldname),
+            'value': value
+        }
+
+    @property
+    def pas_member(self):
+        if not self._pas_member:
+            self._pas_member = getMultiAdapter(
+                (self.context, self.request),
+                name=u'pas_member'
+            )
+        return self._pas_member
+
+    @property
+    def use_view_action(self):
+        registry = getUtility(IRegistry)
+        types_used = registry.get('plone.types_use_view_action_in_listings', [])
+        return types_used
+
+    def normalizeString(self, text):
+        return self.plone_view.normalizeString(text)
+
 
 class ReportView(BrowserView):
     """ Custom class for report view
