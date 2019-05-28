0e6472eabe2a33bcf79dda5c051f9021933b4dee
Petchesi-Iulian <iamnotacat12@gmail.com>
2019-05-28 15:01:27 +0300

    Added monkeypatch for plone toLocalizedTime.

--- eionet/theme/browser/overrides.py
+++ eionet/theme/browser/overrides.py
@@ -1,4 +1,6 @@
 import logging
+from Acquisition import aq_inner
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets.common import PathBarViewlet as ViewletBase
@@ -22,3 +24,19 @@ class PathBarViewlet(ViewletBase):
     def get_home(self):
         tab_action = self.context.portal_actions.get('portal_tabs')
         return tab_action.index_html.title
+
+
+def patched_toLocalizedTime(self, time, long_format=None, time_only=None):
+    """ Monkey patched Convert time to localized time
+    """
+    context = aq_inner(self.context)
+
+    if context.portal_type in ['Collection']:
+        long_format = 0
+
+    util = getToolByName(context, 'translation_service')
+    loc_time = util.ulocalized_time(time, long_format, time_only,
+                                   context=context, domain='plonelocales',
+                                   request=self.request)
+    loc_time = loc_time.replace(',','')
+    return loc_time
